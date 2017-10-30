e8ccdbb72ad259a5779efef94f5642d23899139a
Paul <paul35444@gmail.com>
2017-10-30 17:09:09 -0500

    Added Name prop to app user

--- InventoryApp/Controllers/HomeController.cs
+++ InventoryApp/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using InventoryApp.Models;
+using System.Data.Entity;
 using System.Web.Mvc;
 
 namespace InventoryApp.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+                _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var enteredItems = _context.Items.Include(i => i.Description);
+
+            return View(enteredItems);
         }
 
         public ActionResult About()
